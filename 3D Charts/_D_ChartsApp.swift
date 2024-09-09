d4017b1435784ad1e6ca5f0533b5bec63e261083
Brendan Innis <innisbrendan@gmail.com>
2024-09-09 10:48:02 -0700

    Initial Commit

---
+++ 3D Charts/_D_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_ChartsApp.swift
+//  3D Charts
+//
+//  Created by Brendan Innis on 2024-09-09.
+//
+
+import SwiftUI
+
+@main
+struct _D_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
